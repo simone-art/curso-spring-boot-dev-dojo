816a540b6eda7f176ec0b3c7eec0868d9166fbc7
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-03 17:50:50 -0300

    "Funcionando com sucesso as classes DateUtil e StudentEndpoint com @Autowired e @Component"

--- src/main/java/br/com/devdojo/awesome/ApplicationStart.java
+++ src/main/java/br/com/devdojo/awesome/ApplicationStart.java
@@ -1,9 +1,7 @@
-package br.com.devdojo.awesome.start;
-
+package br.com.devdojo.awesome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /***
@@ -13,9 +11,11 @@ import org.springframework.context.annotation.ComponentScan;
 //Anotação que diz para o Spring que ele tem que configurar as dependências que tem no pom
 @EnableAutoConfiguration
 //Anotação que define o pacote onde está seu endpoint
-@ComponentScan(basePackages = "br.com.devdojo.awesome.endpoint")
+//@ComponentScan(basePackages = "br.com.devdojo.awesome.endpoint") //Não precisa mas desta anotação porque
+//
 
-//@SpringBootApplication
+
+@ComponentScan
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -1,11 +1,14 @@
 package br.com.devdojo.awesome.endpoint;
 
 import br.com.devdojo.awesome.models.Student;
+import br.com.devdojo.awesome.util.DateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static java.util.Arrays.asList;
@@ -13,13 +16,19 @@ import static java.util.Arrays.asList;
 //Anotação do Spring MVC e a diferença do controller, e que o RestController adiciona o @ResponseBody
 //Endpoint: o ponto final onde os usuários acessará a API
 //@RequestMapping, anotação que permite acessar a URL
+// @Autowired faz parte do framework de injecão de dependências do Spring e instância objetos
 
 
 @RestController
 @RequestMapping("student")
 public class StudentEndpoint {
-@RequestMapping(method = RequestMethod.GET, path = "/list")
- public List<Student> listAll(){
-     return asList(new Student("Deku"), new Student("Todoroki"));
- }
+
+    @Autowired
+    private DateUtil dateUtil;
+
+    @RequestMapping(method = RequestMethod.GET, path = "/list")
+    public List<Student> listAll() {
+        System.out.println("-----------" + dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return asList(new Student("Deku"), new Student("Todoroki"));
+    }
 }
