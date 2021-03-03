c1f3df7c11639c8ee6e63afe4c8150d4d6b0f069
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-03 17:51:02 -0300

    "Funcionando com sucesso as classes DateUtil e StudentEndpoint com @Autowired e @Component"

---
+++ src/main/java/br/com/devdojo/awesome/util/DateUtil.java
@@ -0,0 +1,18 @@
+package br.com.devdojo.awesome.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+//Tem que ser colocado o bean @Component porque gera um erro na hora de buildar
+@Component
+public class DateUtil {
+
+    public String formatLocalDateTimeToDatabaseStyle(LocalDateTime localDateTime){
+        return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(localDateTime);
+    }
+}
