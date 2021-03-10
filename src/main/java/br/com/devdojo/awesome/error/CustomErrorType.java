50121d6f481694c8d7c95df4cf6859f2c00122ec
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-10 11:08:11 -0300

    "Criado arquivo para trabalhar os erros"

---
+++ src/main/java/br/com/devdojo/awesome/error/CustomErrorType.java
@@ -0,0 +1,13 @@
+package br.com.devdojo.awesome.error;
+
+public class CustomErrorType {
+     private String errorMensagge;
+
+    public CustomErrorType(String errorMensagge) {
+        this.errorMensagge = errorMensagge;
+    }
+
+    public String getErrorMensagge() {
+        return errorMensagge;
+    }
+}
