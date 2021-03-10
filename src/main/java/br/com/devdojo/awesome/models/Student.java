fd5a55c6003e6c8a4e2f77d669830897bdc04b7b
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-10 10:48:37 -0300

    "Feitas alterações na classe Student"

--- src/main/java/br/com/devdojo/awesome/models/Student.java
+++ src/main/java/br/com/devdojo/awesome/models/Student.java
@@ -1,13 +1,48 @@
 package br.com.devdojo.awesome.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
 public class Student {
+    private int id;
+
     private String name;
 
-    public Student(String name) {
+    //Criada lista static pra evitar instanciar objeto toda vez que se tem um dado novo
+    //Então é melhor trabalhar com uma lista dinámica
+    public static List<Student> studentList;
+
+    //Bloco estático para chamar studentRepository
+    static {
+        studentRepository();
+    }
+
+    public Student(int id, String name) {
+        this(name);
+        this.id = id;
+
+    }
+
+    public Student(String name){
         this.name = name;
     }
 
     public Student() {
+    }
+
+
+    private static void studentRepository(){
+        studentList = new ArrayList<>(asList(new Student("Deku"), new Student("Todoroki")));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
