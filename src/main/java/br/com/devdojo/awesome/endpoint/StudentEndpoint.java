d943f4f5c5985e5de95919811cca6ba3af2e8194
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-10 12:58:22 -0300

    "Criados métodos post, delete e put com sucesso ao verificar no Postman"

--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndpoint.java
@@ -3,6 +3,7 @@ package br.com.devdojo.awesome.endpoint;
 import br.com.devdojo.awesome.error.CustomErrorType;
 import br.com.devdojo.awesome.models.Student;
 import br.com.devdojo.awesome.util.DateUtil;
+import jdk.swing.interop.SwingInterOpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,6 +34,7 @@ public class StudentEndpoint {
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+    //@PathVariable("id") int id, pega atributos por separado
     public ResponseEntity<?> getStudentById(@PathVariable("id") int id){
         Student student = new Student();
         student.setId(id);
@@ -41,6 +43,32 @@ public class StudentEndpoint {
             return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
             return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
         }
+
+        //Método pra enviar id e nome de um novo estudante
+        //Dentro do @RequestBody tem que vir o objeto estudante
+        @RequestMapping(method = RequestMethod.POST)
+        public ResponseEntity<?> save(@RequestBody Student student){
+          Student.studentList.add(student);
+          return new ResponseEntity<>(student, HttpStatus.OK);
+        }
+     //Método pra eliminar um dado
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<?> delete(@RequestBody Student student){
+        Student.studentList.remove(student);
+        System.out.println("Dado deletado com sucesso");
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    //Método pra atualizar um dado
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<?> update(@RequestBody Student student){
+        Student.studentList.remove(student);
+        Student.studentList.add(student);
+        System.out.println("Dado atualizado com sucesso");
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
 
     }
 
