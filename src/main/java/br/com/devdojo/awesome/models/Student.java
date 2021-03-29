a34762d9da99ad5f0530914503842bd84d595c71
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-29 16:04:01 -0300

    "Colocado anotações @NotEmpty e @NotNull que não estão validando"

--- src/main/java/br/com/devdojo/awesome/models/Student.java
+++ src/main/java/br/com/devdojo/awesome/models/Student.java
@@ -2,6 +2,8 @@ package br.com.devdojo.awesome.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -11,7 +13,9 @@ import static java.util.Arrays.asList;
 @Entity
 public class Student extends AbstractEntity {
 
-    @Column
+   @Column
+   @NotNull
+   @NotEmpty(message = "O nome não pode ser vazio")
     private String name;
 
     public String getName() {
