cf4e8feb462f8fbc1e1f5c898f73ceaca5289fe3
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-05 17:35:01 -0300

    "Deletado o CrudRepository e colocado o PaPagingAndSortingRepository pra poder paginar dados de uma database"

--- src/main/java/br/com/devdojo/awesome/repository/StudentRepository.java
+++ src/main/java/br/com/devdojo/awesome/repository/StudentRepository.java
@@ -2,13 +2,14 @@ package br.com.devdojo.awesome.repository;
 
 import br.com.devdojo.awesome.models.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
 //<Student, Long> Student : Entity e o ID
 //CrudRepository é uma interfase
 
-public interface StudentRepository extends CrudRepository<Student, Long> {
+public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
     List<Student> findByNameIgnoreCaseContaining(String name);
     //Student findById(Long id);
 
