9e8a6e881eebd40e431791791fafd3e5b2f51bf9
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-22 12:38:19 -0300

    "Incluido método que configura a autenticação ante de accesar o endopoint "

--- src/main/java/br/com/devdojo/awesome/config/SecurityConfig.java
+++ src/main/java/br/com/devdojo/awesome/config/SecurityConfig.java
@@ -1,14 +1,24 @@
 package br.com.devdojo.awesome.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
+//Método que configura as requisições Http
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
     }
 
+    //Método que configura a autenticação ante de accesar o endopoint
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.inMemoryAuthentication()
+                .withUser("simone").password("devdojo").roles("USER")
+                .and()//Permite o link entre dois usuários
+                .withUser("admin").password("devdojo").roles("USER", "ADMIN");
+    }
 }
