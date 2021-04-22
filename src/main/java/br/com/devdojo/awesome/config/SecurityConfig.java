2d994c9e8176cb9de08a86580929f8c0e3a8568d
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-22 13:18:29 -0300

    "incluído .csrf().disable();no método que configura o HTTTP"

--- src/main/java/br/com/devdojo/awesome/config/SecurityConfig.java
+++ src/main/java/br/com/devdojo/awesome/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.devdojo.awesome.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,13 +11,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 //Método que configura as requisições Http
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@EnableGlobalMethodSecurity(prePostEnabled = true) anotation que faz funcionar o PreAuthorized
+//Incluido no método delete
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .httpBasic()
+                .and()
+                .csrf().disable();
     }
 
     //Método que configura a autenticação ante de accesar o endopoint
